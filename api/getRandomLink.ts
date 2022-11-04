94e59d75d8068bbd995d3104e9dc32bf241c9541
Oryam <48219965+or-yam@users.noreply.github.com>
2022-11-04 15:27:51 +0200

    Fix ts error

--- api/getRandomLink.ts
+++ api/getRandomLink.ts
@@ -3,6 +3,7 @@ import { getLinkMetaData, getSitemapLinks } from '../scraper/sitemap-scraper';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
+  console.log(request);
   const sitemapLinks = await getSitemapLinks();
   const randomLink = sitemapLinks[Math.floor(Math.random() * sitemapLinks.length)];
   const linkMetaData = await getLinkMetaData(randomLink);
