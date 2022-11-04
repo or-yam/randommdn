61fcb4e72fdf4f09245faf8541e135f2fc2a99a1
Oryam <48219965+or-yam@users.noreply.github.com>
2022-11-04 16:28:19 +0200

    Add some error handling

--- api/getRandomLink.ts
+++ api/getRandomLink.ts
@@ -1,11 +1,14 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { getLinkMetaData, getSitemapLinks } from '../scraper/sitemap-scraper';
 
-export default async (request: VercelRequest, response: VercelResponse) => {
-  console.log(request);
-  const sitemapLinks = await getSitemapLinks();
-  const randomLink = sitemapLinks[Math.floor(Math.random() * sitemapLinks.length)];
-  const linkMetaData = await getLinkMetaData(randomLink);
-
-  response.status(200).send(linkMetaData);
+export default async (_request: VercelRequest, response: VercelResponse) => {
+  try {
+    const sitemapLinks = await getSitemapLinks();
+    if (!sitemapLinks.length) response.status(500).send('Failed to fetch links');
+    const randomLink = sitemapLinks[Math.floor(Math.random() * sitemapLinks.length)];
+    const linkMetaData = await getLinkMetaData(randomLink);
+    response.status(200).send(linkMetaData);
+  } catch (error) {
+    response.status(500).send('Something went wrong');
+  }
 };
