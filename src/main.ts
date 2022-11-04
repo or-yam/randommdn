28be4f8873ce97d4eca98fe9b030b7d71259b408
Oryam <48219965+or-yam@users.noreply.github.com>
2022-11-04 14:03:45 +0200

    Use sitemap as first priority

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
-import data from './data.json';
+import { getLinkMetaData, getSitemapLinks } from '../scraper/sitemap-scraper';
+import { LinkMetaData } from '../scraper/types';
+import storedData from './data.json';
 import './style.css';
-
-const numberOfOptions = data.length;
 
 const appElement = document.getElementById('app')!;
 
-const getRandomLink = () => {
+const getRandomLink = async () => {
+  const sitemapLinks = await getSitemapLinks();
+  if (!sitemapLinks.length) {
+    const index = Math.floor(Math.random() * storedData.length);
+    return storedData[index];
+  }
+  const randomLink = sitemapLinks[Math.floor(Math.random() * sitemapLinks.length)];
+  const linkMetaData = await getLinkMetaData(randomLink);
+  return linkMetaData;
+};
+
+const createRandomLink = async () => {
   const prevLink = document.getElementById('link');
   if (prevLink) {
     prevLink.remove();
   }
-  const index = Math.floor(Math.random() * numberOfOptions);
-  const { href, text } = data[index];
+  const linkContent: LinkMetaData = await getRandomLink();
   const link = document.createElement('a');
   link.id = 'link';
   link.target = '_blank';
-  link.href = href;
-  link.innerText = text;
+  link.rel = 'noreferrer';
+  link.href = linkContent.url;
+  link.innerText = linkContent.title;
   appElement.append(link);
 };
 
 const button = document.getElementById('btn')!;
-button.onclick = getRandomLink;
+button.onclick = createRandomLink;
