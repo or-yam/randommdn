7aff7d24f503f0080e42612ca45da38336a2f265
Oryam <48219965+or-yam@users.noreply.github.com>
2022-11-06 11:45:13 +0200

    Refactor + fix special chars escape

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,49 @@
-import axios from 'axios';
-import { LinkMetaData } from '../scraper/types';
-import storedData from './data.json';
-import './style.css';
+import axios from "axios";
+import { LinkMetaData } from "../scraper/types";
+import scrapeData from "./data.json";
+import { replaceXmlSpecialChars } from "./replaceSpecialChars";
+import "./style.css";
+
+const DEFAULT_LINK_DATA: LinkMetaData = {
+  url: "https://developer.mozilla.org",
+  title: "MDN Web Docs",
+  description:
+    "The MDN Web Docs site provides information about Open Web technologies including HTML, CSS, and APIs for both Web sites and progressive web apps.",
+  tag: "WEB",
+};
+
+const buttonElement = document.getElementById("btn")! as HTMLButtonElement;
+const linkElement = document.getElementById("link")! as HTMLAnchorElement;
+const descriptionElement = document.getElementById("description")! as HTMLParagraphElement;
+const tagElement = document.getElementById("tag")! as HTMLHeadingElement;
+const resultsContainer = document.getElementById("results")! as HTMLDivElement;
+const loaderElement = document.getElementById("loader")! as HTMLDivElement;
 
 const getRandomLink = async (): Promise<LinkMetaData> => {
   try {
-    const linkMetaData = (await axios.get<LinkMetaData>('/api/getRandomLink')).data;
+    const linkMetaData = (await axios.get<LinkMetaData>("/api/getRandomLink")).data;
     return linkMetaData;
   } catch (error) {
-    console.log('Data from stored json', error);
-    const index = Math.floor(Math.random() * storedData.length);
-    return storedData[index];
+    console.log("Retrieved data from scraped json", error);
+    const index = Math.floor(Math.random() * scrapeData.length);
+    return scrapeData[index];
   }
 };
 
-const buttonElement = document.getElementById('btn')! as HTMLButtonElement;
-const linkElement = document.getElementById('link')! as HTMLAnchorElement;
-const descriptionElement = document.getElementById('description')! as HTMLParagraphElement;
-const tagElement = document.getElementById('tag')! as HTMLHeadingElement;
-const resultsContainer = document.getElementById('results')! as HTMLDivElement;
-const loaderElement = document.getElementById('loader')! as HTMLDivElement;
-
-const createRandomLink = async () => {
-  resultsContainer.style.display = 'none';
-  loaderElement.style.display = 'block';
-
-  const linkContent: LinkMetaData = await getRandomLink();
-
-  linkElement.href = linkContent.url || 'https://developer.mozilla.org';
-  linkElement.innerText = linkContent.title || 'MDN Web Docs';
-  descriptionElement.innerText =
-    linkContent.description ||
-    'The MDN Web Docs site provides information about Open Web technologies including HTML, CSS, and APIs for both Web sites and progressive web apps.';
-  tagElement.innerText = linkContent.tag ? `-${linkContent.tag}-` : '-WEB-';
-  resultsContainer.style.display = 'block';
-  loaderElement.style.display = 'none';
+const setLoadingAnimation = (showLoadingAnimation: boolean) => {
+  resultsContainer.style.display = showLoadingAnimation ? "none" : "block";
+  loaderElement.style.display = showLoadingAnimation ? "block" : "none";
 };
 
+const createRandomLink = async () => {
+  setLoadingAnimation(true);
+  const { url, title, description, tag }: LinkMetaData = await getRandomLink();
+  linkElement.href = replaceXmlSpecialChars(url || DEFAULT_LINK_DATA.url);
+  linkElement.innerText = replaceXmlSpecialChars(title || DEFAULT_LINK_DATA.title);
+  descriptionElement.innerText = replaceXmlSpecialChars(description || DEFAULT_LINK_DATA.description);
+  tagElement.innerText = replaceXmlSpecialChars(`-${tag || DEFAULT_LINK_DATA.tag}-`);
+  setLoadingAnimation(false);
+};
+
+window.onload = createRandomLink;
 buttonElement.onclick = createRandomLink;
---
+++ src/replaceSpecialChars.ts
@@ -0,0 +1,8 @@
+export const replaceXmlSpecialChars = (string: string) => {
+  return string
+    .replace(/&amp;/g, "&")
+    .replace(/&gt;/g, ">")
+    .replace(/&lt;/g, "<")
+    .replace(/&quot;/g, '"')
+    .replace(/&apos;/g, "'");
+};
